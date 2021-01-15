4cb210eb0070ad00f6a8b66279793994c703a045
rawrzz-m <77323321+rawrzz-m@users.noreply.github.com>
2021-01-15 14:07:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from random import randint
 from youtubesearchpython import SearchVideos
 from multiprocessing import Process
 
-bot = telepot.Bot("API_TOKEN")
+bot = telepot.Bot("1426906186:AAGZk5qMoFWFTsXxJjYbhjaIb86qQBHpfVM")
 class Chat:
 	def __init__(self, chat_id, text, msg):
 		self.msg = msg
